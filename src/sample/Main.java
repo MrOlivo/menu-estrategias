b8ece6b7487a51426abab6dfe0b6831174191de0
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2020-11-18 15:52:03 -0700

    Update Main.java
    Remove org.jetbrains.annotations package

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,19 +5,17 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
-import org.jetbrains.annotations.NotNull;
 
 public class Main extends Application {
 
     @Override
-    public void start(@NotNull Stage primaryStage) throws Exception {
+    public void start(Stage primaryStage) throws Exception {
         FXMLLoader fxmlloader = new FXMLLoader();
         Parent root = fxmlloader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Menú de estrategias");
         primaryStage.setScene(new Scene(root, 300, 500));
         primaryStage.show();
     }
-
 
     public static void main(String[] args) {
         launch(args);
