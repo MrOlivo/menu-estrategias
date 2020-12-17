db5d86a190f157ca68bdc8373b2e349463327fce
Edwin Olivo <46498127+MrOlivo@users.noreply.github.com>
2020-12-17 09:35:10 -0700

    Refactor functions and variables

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -18,58 +18,57 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class Controller implements Initializable {
-    public ComboBox choice_conduccion;
-    public ComboBox choice_neumatico;
-    public TableView table_resultados;
-    public TableColumn column_id;
-    public TableColumn column_desgaste;
-    public Button button_calcular;
+    public ComboBox<String> combo_driving_type;
+    public ComboBox<String> combo_tire;
+    public TableView<Registro> table_results;
+    public TableColumn<Registro, Integer> column_id;
+    public TableColumn<Registro, String> column_wear;
+    public Button button_calculate;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
         ObservableList<String> compuestos = FXCollections.observableArrayList();
         compuestos.addAll("Blando", "Superblando", "Ultrablando");
-        choice_neumatico.setItems(compuestos);
-        choice_neumatico.getSelectionModel().selectFirst();
+        combo_tire.setItems(compuestos);
+        combo_tire.getSelectionModel().selectFirst();
 
         ObservableList<String> modos = FXCollections.observableArrayList();
         modos.addAll("Neutral", "Atacar", "Conservar");
-        choice_conduccion.setItems(modos);
-        choice_conduccion.getSelectionModel().selectFirst();
+        combo_driving_type.setItems(modos);
+        combo_driving_type.getSelectionModel().selectFirst();
 
         column_id.setCellValueFactory(new PropertyValueFactory<>("id"));
-        column_desgaste.setCellValueFactory(new PropertyValueFactory<>("desgaste"));
+        column_wear.setCellValueFactory(new PropertyValueFactory<>("desgaste"));
 
-        button_calcular.setOnAction(actionEvent -> {
+        button_calculate.setOnAction(actionEvent -> {
             try {
-                llenarTabla();
+                fillTable();
             } catch (Exception e) {
                 e.printStackTrace();
             }
         });
     }
 
-    public void llenarTabla() throws Exception {
-        table_resultados.getItems().clear();
+    public void fillTable() {
+        table_results.getItems().clear();
 
-        ITipoConduccion tipoConduccion = elegirConduccion((String) choice_conduccion.getValue());
-
-        String compuesto = (String) choice_neumatico.getValue();
-        INeumatico neumatico = PirelliFactory.ObtenerNeumatico(compuesto);
+        ITipoConduccion drivingStyle = getDrivingStyle(combo_driving_type.getValue());
+        String compound = combo_tire.getValue();
+        INeumatico tire = PirelliFactory.ObtenerNeumatico(compound);
 
         int i = 0;
         float vidaUtil = 100f;
 
         while (vidaUtil > 0) {
-            vidaUtil = tipoConduccion.calcularDesgaste(neumatico, vidaUtil);
+            vidaUtil = drivingStyle.calcularDesgaste(tire, vidaUtil);
             if (vidaUtil <= 0) {
                 break;
             }
-            table_resultados.getItems().add(new Registro(i += 1, vidaUtil));
+            table_results.getItems().add(new Registro(i += 1, vidaUtil));
         }
     }
 
-    private ITipoConduccion elegirConduccion(String opt){
+    private ITipoConduccion getDrivingStyle(String opt){
         switch (opt) {
             case "Conservar":
                 return new Conservar();
