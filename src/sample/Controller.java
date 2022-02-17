47ba42f1a234a33317af373223f8f3a102a8c02d
Olivo <46498127+MrOlivo@users.noreply.github.com>
2022-02-16 17:04:32 -0700

    Rename variables

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -18,29 +18,29 @@ import javafx.scene.control.TableView;
 import javafx.scene.control.cell.PropertyValueFactory;
 
 public class Controller implements Initializable {
-    public ComboBox<String> combo_driving_style;
-    public ComboBox<String> combo_tire;
-    public TableView<Registro> table_results;
+    public ComboBox<String> comboBoxDrivingStyles;
+    public ComboBox<String> comboBoxTyres;
+    public TableView<Registro> tableviewResults;
     public TableColumn<Registro, Integer> column_id;
     public TableColumn<Registro, String> column_wear;
-    public Button button_calculate;
+    public Button buttonCalculate;
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-        ObservableList<String> compound_list = FXCollections.observableArrayList();
-        compound_list.addAll("Soft", "Super soft", "Ultra soft");
-        combo_tire.setItems(compound_list);
-        combo_tire.getSelectionModel().selectFirst();
+        ObservableList<String> compoundsList = FXCollections.observableArrayList();
+        compoundsList.addAll("Soft", "Super-soft", "Ultra-soft");
+        comboBoxTyres.setItems(compoundsList);
+        comboBoxTyres.getSelectionModel().selectFirst();
 
         ObservableList<String> driving_list = FXCollections.observableArrayList();
-        driving_list.addAll("Neutral", "Atacar", "Conservar");
-        combo_driving_style.setItems(driving_list);
-        combo_driving_style.getSelectionModel().selectFirst();
+        driving_list.addAll("Neutral", "Attack", "Keep");
+        comboBoxDrivingStyles.setItems(driving_list);
+        comboBoxDrivingStyles.getSelectionModel().selectFirst();
 
         column_id.setCellValueFactory(new PropertyValueFactory<>("id"));
-        column_wear.setCellValueFactory(new PropertyValueFactory<>("desgaste"));
+        column_wear.setCellValueFactory(new PropertyValueFactory<>("wear"));
 
-        button_calculate.setOnAction(actionEvent -> {
+        buttonCalculate.setOnAction(actionEvent -> {
             try {
                 fillTable();
             } catch (Exception e) {
@@ -50,28 +50,18 @@ public class Controller implements Initializable {
     }
 
     public void fillTable() {
-        table_results.getItems().clear();
-
-        ITipoConduccion drivingStyle = getDrivingStyle(combo_driving_style.getValue());
-        INeumatico tire = PirelliFactory.getTyre(combo_tire.getValue());
+        ITipoConduccion drivingStyle = ITipoConduccion.getDrivingStyle(comboBoxDrivingStyles.getValue());
+        INeumatico tyre = PirelliFactory.getTyre(comboBoxTyres.getValue());
 
         int i = 0;
-        float vidaUtil = 100f;
+        float useful_life = 100f;
+        ObservableList<Registro> tempList = FXCollections.observableArrayList();
 
         do{
-            table_results.getItems().add(new Registro(i++, vidaUtil));
-            vidaUtil = drivingStyle.calcularDesgaste(tire, vidaUtil);
-        } while (vidaUtil > 0);
-    }
+            tempList.add(new Registro(i++, useful_life));
+            useful_life = drivingStyle.calculateWearing(tyre, useful_life);
+        } while (useful_life > 0);
 
-    private ITipoConduccion getDrivingStyle(String opt){
-        switch (opt.toLowerCase()) {
-            case "conservar":
-                return new Conservar();
-            case "atacar":
-                return new Atacar();
-            default:
-                return new Neutral();
-        }
+        tableviewResults.setItems(tempList);
     }
 }
